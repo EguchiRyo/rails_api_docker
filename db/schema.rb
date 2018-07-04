8b0a474e197dc02459b2bce71688a578e18d5156
EguchiRyo <eguchi@to-on.com>
2018-07-04 21:20:20 +0900

    Add column to user, enable to update param

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180614100447) do
+ActiveRecord::Schema.define(version: 20180704112729) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
+    t.string "kana"
   end
 
 end
